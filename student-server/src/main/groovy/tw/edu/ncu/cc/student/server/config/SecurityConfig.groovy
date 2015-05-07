98fa957c038bf61e48d1973b3ff27dfa411ba2df
jasonChiu <jason1122g@gmail.com>
2015-05-07 20:41:06 +0800

    add java configs

---
+++ student-server/src/main/groovy/tw/edu/ncu/cc/student/server/config/SecurityConfig.groovy
@@ -0,0 +1,49 @@
+package tw.edu.ncu.cc.student.server.config
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import tw.edu.ncu.cc.oauth.resource.filter.ApiTokenDecisionFilter
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity( prePostEnabled = true )
+public class SecurityConfig {
+
+    @Order( 1 )
+    @Configuration
+    public static class OauthGuard extends WebSecurityConfigurerAdapter {
+
+        @Autowired
+        def ApiTokenDecisionFilter apiTokenDecisionFilter
+
+        @Override
+        protected void configure( HttpSecurity http ) throws Exception {
+            http.antMatcher( "/v*/cards" )
+                    .addFilterAfter( apiTokenDecisionFilter, UsernamePasswordAuthenticationFilter.class )
+                    .csrf().disable()
+        }
+    }
+
+    @Order( 2 )
+    @Configuration
+    public static class ManagementAPI extends WebSecurityConfigurerAdapter {
+
+        @Value( '${custom.management.security.access}' )
+        def String managementAccess
+
+        @Override
+        protected void configure( HttpSecurity http ) throws Exception {
+            http.antMatcher( "/management/**" )
+                    .authorizeRequests()
+                        .anyRequest().access( managementAccess )
+        }
+
+    }
+
+}
